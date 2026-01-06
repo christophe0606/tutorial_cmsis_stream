97a7f20b03ff1fd7fd4e9522868676b20daf8a9d
Christophe Favergeon <Christophe.Favergeon@arm.com>
2026-01-06 13:12:18 +0100

    Added example for in-place buffer modification when the buffer is not shared.

---
+++ example5/AppNodes.hpp
@@ -0,0 +1,189 @@
+#pragma once
+
+#include "StreamNode.hpp"
+
+using namespace arm_cmsis_stream;
+
+// Template describes the numpert of IO ports.
+// For each port there is the datatype and the number of samples
+// Inputs are listed first, then outputs.
+template <typename OUT, int outputSize>
+class Source : public GenericSource<OUT, outputSize>
+{
+public:
+    Source(FIFOBase<OUT> &dst) : GenericSource<OUT, outputSize>(dst) {};
+
+    int run() final
+    {
+        // Get output buffer. The code does not see the FIFO.
+        // It only sees a buffer to write to.
+        // Call to getWriteBuffer is modifying the FIFO state so should be called only once
+        // in the run() method.
+        OUT *b = this->getWriteBuffer();
+
+        std::cout << "Source" << std::endl;
+        for (int i = 0; i < outputSize; i++)
+        {
+            b[i] = (OUT)i;
+        }
+        return (CG_SUCCESS);
+    };
+};
+
+// Generic template declaration for sink node
+template <typename IN, int inputSize>
+class ProcessingNode : public GenericSink<IN, inputSize>
+{
+    // To limit the number of execution in the tutorial (otherwise it executes forever)
+    static constexpr int maxNbRuns_ = 5;
+
+public:
+    enum selector
+    {
+        selNumber = 0
+    };
+    static std::array<uint16_t, 1> selectors;
+    /* Constructor needs the input and output FIFOs */
+    ProcessingNode(FIFOBase<IN> &src) : GenericSink<IN, inputSize>(src) {};
+
+    /*
+       Node processing
+       1 is added to the input
+    */
+    int run() final
+    {
+        std::cout << "ProcessingNode" << std::endl;
+        IN *a = this->getReadBuffer();
+
+        // This will allocate memory with malloc
+        // It is possible to provide a custom deleter to use another way to free the memory
+        // It is also possible to provide a pointer to an existing memory area and the corresponding deleter
+        // This class is otherwise similar to std::unique_ptr but the deleter does not appear in the
+        // template parameters and so it can be used more easily in events.
+        UniquePtr<IN> ptr(inputSize);
+
+        // We write data into the unique pointer buffer
+        for (int i = 0; i < inputSize; i++)
+        {
+            ptr[i] = a[i] + 1;
+        }
+
+        // Before exchanging this buffer with other nodes, it needs to be wrapped
+        // in an object doing atomic reference counting.
+        // It is similar to std::shared_ptr but the reference counting update are protected with a mutex.
+        // The class ProtectedBuffer is generally used either for RawBuffer or Tensor so
+        // aliases and convenience methods are provided to wrap the unique pointer in a protected buffer.
+
+        // We create a 1D tensor with inputSize elements of type IN.
+        // TensorPtr is an alias to a ProtectedBuffer<Tensor<IN>>
+
+        TensorPtr<IN> t = TensorPtr<IN>::create_with((uint8_t)1,
+                                                     cg_tensor_dims_t{inputSize},
+                                                     std::move(ptr));
+
+        // Send a Number event in an asynchronous way. Event is sent on output event port 0
+        // The event is put into the event queue and processed later by the event thread
+        // The event thread always has lower priority than the dataflow thread
+        // Note that sending this event in a synchronous way is not advised here
+        // since the dataflow should not be blocked by event processing and we do not
+        // know how much time it will take to process the event in the receiving nodes.
+
+        ev0.sendAsync(kNormalPriority, selectors[selNumber], std::move(t)); // Send a async Number event to all subscribers
+        // If there is only one subscriber the event queue will move the event
+        // If there are several subscribers, the event queue will clone the events
+        // which will increase the ref count of the ProtectedBuffer but won't copy the UniquePtr data
+
+        nbRun_++;
+        if (nbRun_ >= maxNbRuns_)
+        {
+            // If some events are still in the event queue they will may not be processed.
+            // As soon as the stream thread has quitted, the event queue processing is stopped.
+            return (CG_STOP_SCHEDULER);
+        }
+
+        return (CG_SUCCESS);
+    };
+
+    // At init, when a new node is connected to an output port this function is called.
+    // Here we only have one output event port (port 0) so we do not check dstPort
+    // to know in which output queue the node must be put.
+    // By default, we put all connected nodes to output queue ev0.
+    void subscribe(int outputPort, StreamNode &dst, int dstPort) final
+    {
+        ev0.subscribe(dst, dstPort);
+    }
+
+protected:
+    EventOutput ev0;
+    int nbRun_ = 0;
+};
+
+class Sink : public StreamNode
+{
+
+public:
+    enum selector
+    {
+        selNumber = 0
+    };
+    static std::array<uint16_t, 1> selectors;
+
+    Sink() : StreamNode() {};
+
+    void processTensor(TensorPtr<float> v)
+    {
+        std::cout << "Sink received value: " << std::endl;
+        // To access the data we need to lock the protected buffer. We can use a write lock or a
+        // read lock.
+        // Here we only want to read it:
+
+        bool lockError;
+        v.lock(lockError, [](bool isShared, Tensor<float> &tensor)
+               {
+            float *buf = tensor.buffer();
+            // If buffer is shared we cannot modify it inplace
+            if (isShared)
+            {
+                 for (size_t i = 0; i < 5; i++)
+                 {
+                     std::cout << (buf[i] + 1) << std::endl;
+                 }
+            }
+            else
+            {
+                // Inplace modification since we are the only owner of the buffer
+                for (size_t i = 0; i < 5; i++)
+                {
+                    buf[i] = buf[i] + 1;
+                }
+                for (size_t i = 0; i < 5; i++)
+                {
+                    std::cout << buf[i] << std::endl;
+                }
+            }
+            std::cout << std::endl; });
+    }
+
+    // When a new event is received on any of the input ports, this function is called.
+    // dstPort is the port number receiving the event.
+    // This function may be called from different thread but to simplify things
+    // one could impose that only the event thread can call it.
+    // So if an event must be sent to the node from another source (the application), it should go to
+    // the event queue rather than use this function directly. It has an overhead but
+    // simplifies the implementation of this function. It is always run from event thread and
+    // since there is only one event thread in this design there is no concurrency issue.
+    // A more complex design may use a thread pool to process events in parallel
+    void processEvent(int dstPort, Event &&evt) final override
+    {
+        if (evt.event_id == selectors[selNumber])
+        {
+            if (evt.wellFormed<TensorPtr<float>>())
+            {
+                evt.apply<TensorPtr<float>>(&Sink::processTensor, *this);
+            }
+        }
+    }
+
+protected:
+    int nbRun_ = 0;
+};
---
+++ example5/create.py
@@ -0,0 +1,128 @@
+# Include definitions from the Python package
+from cmsis_stream.cg.scheduler import GenericNode,GenericSink,GenericSource,BaseNode
+from cmsis_stream.cg.scheduler import Graph,CType,F32,Configuration
+from cmsis_stream.cg.scheduler.graphviz import Style
+
+import subprocess
+
+########################
+### Define new types of Nodes here
+
+class Source(GenericSource):
+    """Source node producing data of a given type and length"""
+    def __init__(self,name,theType,outLength):
+        GenericSource.__init__(self,name)
+        self.addOutput("o",theType,outLength)
+
+    @property
+    def typeName(self):
+        """The name of the C++ class implementing this node"""
+        return "Source"
+
+# Processing node is now a dataflow sink since it only has a dataflow input
+# and no dataflow output.
+# The output is an event output
+class ProcessingNode(GenericSink):
+    """Processing node with one input and one event output"""
+    def __init__(self,name,theType,inLength):
+        GenericSink.__init__(self,name,selectors=["number"])
+        self.addInput("i",theType,inLength)
+        self.addEventOutput(1)
+
+    @property
+    def typeName(self):
+        """The name of the C++ class implementing this node"""
+        return "ProcessingNode"
+
+# The Sink is now a pure event node without any dataflow port
+class Sink(BaseNode):
+    """Sink node consuming events"""
+    def __init__(self,name):
+        BaseNode.__init__(self,name,selectors=["number"])
+        self.addEventInput(1)
+
+    @property
+    def typeName(self):
+        """The name of the C++ class implementing this node"""
+        return "Sink"
+    
+
+
+########################
+### Creation of the graph
+
+# Define a float datatype
+floatType=CType(F32)
+
+# Create the nodes with io datatype and number of sample produces / consumed
+src=Source("source",floatType,128)
+processing=ProcessingNode("processing",floatType,128)
+sink=Sink("sink")
+
+
+# Create a Graph object
+the_graph = Graph()
+
+# Connect the source to the processing node
+the_graph.connect(src.o,processing.i)
+
+# Connecting events must use the array syntax with a string.
+# The name of events if iev for inpiut and event port number or
+# oev for output and event port number.
+the_graph.connect(processing["oev0"],sink["iev0"])
+
+
+# You may try to insert the adder node between source and processing node
+
+#####################################
+# Generation of the C++ wrapper code and initialization code
+conf = Configuration()
+# Don't use float32_t for float datatype but use the standard float (so that CMSIS-DSP is not needed)
+conf.CMSISDSP = False
+# Synchronous execution (asynchronous mode is deprecated)
+conf.asynchronous = False
+# Layout of the generated graph picture
+conf.horizontal=True
+# Generate ID and macros to identify the nodes from outside the graph
+# Needed if the nodes must be controlled from outside the graph
+conf.nodeIdentification = True
+# Name of scheduler function
+conf.schedName = "scheduler"
+# Memory optimization of buffer for dataflow (require buffer to be aligned on 8 bytes)
+conf.memoryOptimization = True
+# Prefix to add before name of buffer. By default the names are buf0, buf1 but
+# it can conflict with other variables in the project
+conf.prefix = "stream"
+
+# Compute the dataflow scheduling
+scheduling = the_graph.computeSchedule(config=conf)
+
+# Schedule length = number of states in generated scheduler state machine
+# If this number is too big it generally means that some
+# input / output lengths are too mismatched and may indicate and error in the graph
+# description
+print(f"Schedule length = {scheduling.scheduleLength}")
+# Memory usage of the FIFOs : to check the effect of memory optimization
+# (For some graphs, memory optimization is not possible and memory usage remains the same)
+print(f"Memory usage {scheduling.memory} bytes")
+
+# Generation of the files
+# Generate the C++ state machine and initialization code in current directory
+scheduling.ccode(".",conf)
+
+# Optional : generate some json data
+# It can be used by external tools. For instance, one coudlcould imagine a PC sending an event
+# to a graph running on a board. In that case, the node ID and selector ID must be known
+# The json is giving a mapping from string to ID.
+# This mapping is graph dependent
+scheduling.genJsonIdentification("./description",conf)
+scheduling.genJsonSelectors("./description",conf)
+scheduling.genJsonSelectorsInit("./description",conf)
+
+# Generate hte graphviz file for visualization of the graph
+myStyle = Style()
+with open("./description/graph.dot","w") as f:
+        scheduling.graphviz(f,style=myStyle)
+
+# Optional : if graphviz tools installed you can generate a png or pdf
+subprocess.run(["dot","-Tpng","./description/graph.dot","-o","./graph.png"])
